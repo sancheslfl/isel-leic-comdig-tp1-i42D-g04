b9261ba3cd35981ba89fc80fa3d8935ff61abbcc
TomasPereira05 <A49486@alunos.isel.pt>
2023-03-23 15:00:08 +0000

    ficheiro C e PY

---
+++ C.c
@@ -0,0 +1,17 @@
+int count_ones( int val ){// retornam o número de bits a 1 e a 0 no valor inteiro val, passado como parâmetro.
+}
+int count_zeros( int val ){// retornam o número de bits a 1 e a 0 no valor inteiro val, passado como parâmetro.
+}
+void print_bits( int val ){//a qual imprime como caracteres os valores dos bits de todos os elementos fo valor inteiro val, passado como parâmetro.
+
+}
+char most_frequent_symbol( char *file_name ){// a qual retorna o símbolo mais frequente do ficheiro
+//de texto file_name. A função imprime na consola o número de vezes que esse símbolo ocorre no ficheiro.
+
+}
+void negative_file( char *input_file_name, char *output_file_name){//a qual transforma o ficheiro
+//de entrada input_file_name no ficheiro de saída output_file_name. O ficheiro de saída é produzido a partir do
+//ficheiro de entrada através na negação de cada bit do mesmo.
+
+
+}
