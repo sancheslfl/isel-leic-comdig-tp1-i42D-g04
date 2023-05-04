67500c3ec051d0cd8795aeb1c7e772815e6af455
TomasPereira05 <A49486@alunos.isel.pt>
2023-05-04 16:36:03 +0100

    Ex5a completo

--- EX5a.py
+++ EX5a.py
@@ -1,18 +1,25 @@
 import numpy as np
 
-def output():
-    str = "Exemplodetransmissaointerleaving"
-    arr = list(str)
-    print(arr)
-    collums = 4
-    lines = 8
-    mz = matriz(arr,lines,collums)
-    #interleaving= []
-    #for i in range(0,collums):
-     #   interleaving = np.array([mz[:,i]])
-     #   print(interleaving)
+def interleaving(matrizs):
+    output = ''
+    mz = matriz(matrizs,lines,collums)
+    for i in range(0,collums):
+        for j in range(0,lines):
+            output += mz[j,i]
+    return output
 
 def matriz(arr,lines,collums):
     list = np.array(arr).reshape(lines,collums)
+    print("Matriz:")
     print(list)
     return list
+
+#Exemplo 1
+str = "ExemploDeTransmissaoInterleaving"
+print("String:", str)
+arr = list(str)
+#print("array:", arr)
+collums = 4
+lines = 8
+print("NºLinhas: {} e NºColunas: {}".format(lines, collums))
+print("Interleaving:", interleaving(arr))
